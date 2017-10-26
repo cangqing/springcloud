b11d36cb5f76ec012dd2eada287bffff0daed77e
闻乃松 <wennaisong@gooagoo.com>
2017-10-26 11:49:07 +0800

    add start command

--- dockereurakaregister/run.cmd
+++ dockereurakaregister/run.cmd
@@ -1,1 +1,2 @@
+mvn clean package docker:build
 docker run --link dockereurekaserver -p 8763:8763 -t wennaisong/dockereurakaregister
--- dockereurekaserver/run.cmd
+++ dockereurekaserver/run.cmd
@@ -1,1 +1,2 @@
+mvn clean package docker:build
 docker run --name dockereurekaserver -p 8761:8761 -t wennaisong/dockereurekaserver
